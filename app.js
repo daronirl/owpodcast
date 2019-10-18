1607729403daedae9a6302896e9071aae4b3a2d5
John Prescott <jprescottirl@gmail.com>
2019-10-17 21:59:44 -0700

    added jQuery  for effects

---
+++ app.js
@@ -0,0 +1,3 @@
+$('.navLink').hide().fadeIn(2000);
+$('.main').hide().slideDown(2000);
+$('.episode').hide().slideDown(2000).delay(3000);
---
+++ script.js
@@ -0,0 +1,6 @@
+const main = document.getElementsByTagName('h1');
+
+main.addEventListener(('mouseover', () => {
+    main.textContent = footer.textContent.toUpperCase();
+
+});
